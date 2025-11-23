8f7ea8b5644d98623fc5c54defe51552973b434d
Sufiyan Khan <suffkhan13@gmail.com>
2025-11-23 01:32:55 -0600

    Add multiple STUN servers for better connectivity

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -22,7 +22,9 @@ var connections = {};
 
 const peerConfigConnections = {
     "iceServers": [
-        { "urls": "stun:stun.l.google.com:19302" }
+        { "urls": "stun:stun.l.google.com:19302" },
+        { "urls": "stun:stun1.l.google.com:19302" },
+        { "urls": "stun:stun2.l.google.com:19302" }
     ]
 }
 
